dbeefaa0a0105020c399730dc8b6efcb8fa71f38
jesse keane <me@jessekeane.me>
2014-08-02 11:05:09 -0700

    add a test

--- test/index.js
+++ test/index.js
@@ -11,6 +11,15 @@ test('plucks values at specified key', function(t) {
   t.deepEqual(pluck('id', arr), expected)
 })
 
+test('supports deep-plucking', function(t) {
+  t.plan(1)
+
+  var arr = [{deep: {id: 1}}, {deep: {id: 2}}, {deep: {id: 3}}]
+    , expected = [1, 2, 3]
+
+  t.deepEqual(pluck('deep.id', arr), expected)
+})
+
 test('returns array if nothing pluckable', function(t) {
   t.plan(1)
 
